242156df44c861f6e7b1bfa3155c882f80b94a72
ochoachristian <72667987+ochoachristian@users.noreply.github.com>
2022-09-03 20:23:16 -0400

    Update ServerController.java

--- Module-2-capstone/module-2-capstone/tenmo-server/src/main/java/com/tenmo/controller/ServerController.java
+++ Module-2-capstone/module-2-capstone/tenmo-server/src/main/java/com/tenmo/controller/ServerController.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 @RestController
 @PreAuthorize("isAuthenticated()")
-//@RequestMapping
 public class ServerController {
     private AccountDao accountDao;
     private TransferDao transferDao;
